87c174a240234789199d740a6c24b1c09f6d5cfc
zhiquan <x.zhiquan@gmail.com>
2020-07-19 17:26:26 +0800

    use router function to replace the direct db operation.

--- routers/say/hello/route.js
+++ routers/say/hello/route.js
@@ -8,15 +8,7 @@ router.post(
 
 router.get(
     "/author",
-    async (req, res, next) => {
-        // const authors = await router.mdl.models.author.find({});
-        // res.endWithData(authors);
-        res.addData({authors: await (router.Model('author')).find({})})
-        // res.addData(await (router.Model('author')).find({}))
-
-        return next();
-    },
-    // router.FindDocuments("author")
+    router.FindDocuments("author")
 );
 
 module.exports = router;
