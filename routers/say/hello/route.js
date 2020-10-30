21d8d7258ed6413b9c651d16a4fa930d530b77b8
Zhiquan <x.zhiquan@gmail.com>
2020-10-30 13:34:46 +0800

    use project root to import express

--- routers/say/hello/route.js
+++ routers/say/hello/route.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const path = require("path");
+const express = require(path.resolve('./') + "/node_modules/express");
 const router = express.Router();
 
 router.post(
